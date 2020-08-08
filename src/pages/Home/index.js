09522bc2c2fb0de47628c06edb88bbcd90be85a4
Heitor Rabello <he1tor@hotmail.com>
2020-08-08 16:25:49 -0300

    Add Carousel

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,21 +1,39 @@
 import React from 'react';
 
-import PageDefault from '../../components/PageDefault';
 import dadosIniciais from '../../data/dados_iniciais.json';
+import PageDefault from '../../components/PageDefault';
 import BannerMain from '../../components/BannerMain';
 import Carousel from '../../components/Carousel';
 
 function Home() {
     return (
-      <div>
-        <PageDefault>
-          <BannerMain 
-            vidoTitle={dadosIniciais.categorias[0].videos[0].titulo}
-            url={dadosIniciais.categorias[0].videos[0].url}
-            videoDescription={"O que é o front End?"}
-          />
-        </PageDefault>
-      </div>
+      <PageDefault>
+        <BannerMain 
+          vidoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+          url={dadosIniciais.categorias[0].videos[0].url}
+          videoDescription={"O que é o front End?"}
+        />
+
+        {dadosIniciais.categorias.map((categoria) => {
+          console.log(categoria);
+          if (categoria.titulo === "Front End"){
+            return(
+              <Carousel 
+                ignoreFirstVideo
+                key={categoria.titulo}
+                category={categoria} 
+              />
+            )
+          }
+
+          return(
+            <Carousel 
+              key={categoria.titulo}
+              category={categoria} 
+            />
+          )
+        })}
+      </PageDefault>
     );
   }
 
