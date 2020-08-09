c9c9f517166181958996a4f03242cb0ecca440ff
Heitor Rabello <he1tor@hotmail.com>
2020-08-09 16:17:50 -0300

    :lipstick: Carrossel

--- src/components/Carrossel/styles.js
+++ src/components/Carrossel/styles.js
@@ -1,5 +1,12 @@
 import styled from 'styled-components';
 
+export const VideoCardGroupContainer = styled.section`
+  color: white;
+  min-height: 197px;
+  margin-left: 5%;
+  margin-bottom: 16px;
+`;
+
 export const Title = styled.h3`
   font-style: normal;
   font-weight: normal;
@@ -31,25 +38,4 @@ export const ExtraLink = styled.a`
     margin-bottom: 16px;
     margin-left: 0;
   }
-`;
-
-export const VideoCardList = styled.ul`
-  margin: 0;
-  padding-left: 0;
-  padding-bottom: 32px;
-  list-style: none;
-  display: flex;
-  overflow-x: auto;
-  flex-direction: row;
-  
-  li {
-    margin-right: 16px;
-  }
-`;
-
-export const VideoCardGroupContainer = styled.section`
-  color: white;
-  min-height: 197px;
-  margin-left: 5%;
-  margin-bottom: 16px;
 `;
